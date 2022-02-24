ffccbc8bc44e89344830149d5bdc69068749dec1
ShengZhe0820 <55240664+ShengZhe0820@users.noreply.github.com>
2022-02-24 17:30:20 +0800

    Revert to b0bbcf1f7

---
+++ TeamSportZ/Assets/Scripts/Manager/ScoreManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI hiScoreText;
+    public TextMeshProUGUI coin;
+
+    private float scoreCount;
+    private float hiScoreCount;
+    private int coinCount;
+
+    public float pointsPerSecond;
+
+    private bool increaseScore = true;
+
+    public void increaseCoinCount()
+    {
+        coinCount++;
+        coin.text = "Coins: " + coinCount;
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            hiScoreCount = PlayerPrefs.GetFloat("HighScore");
+            hiScoreText.text = "Hi-Score: " + Mathf.Round(hiScoreCount);
+        }
+        coin.text = "Coins: " + coinCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (increaseScore)
+        {
+            scoreCount += pointsPerSecond * Time.deltaTime;
+        }
+
+        if (scoreCount > hiScoreCount)
+        {
+            hiScoreCount = scoreCount;
+            hiScoreText.text = "Hi-Score: " + Mathf.Round(hiScoreCount);
+            PlayerPrefs.SetFloat("HighScore", hiScoreCount);
+        }
+
+        scoreText.text = "Score: " + Mathf.Round(scoreCount);
+        
+    }
+}
