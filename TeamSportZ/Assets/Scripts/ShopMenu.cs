592ace0387cdd281c16ca997fd70b27ade8b087c
chia0425 <ryanchiascsp15@gmail.com>
2022-02-25 22:32:54 +0800

    heart bar included in the for game session
    health script
    health = current health
    hearts = max capacity of hearts

--- TeamSportZ/Assets/Scripts/ShopMenu.cs
+++ TeamSportZ/Assets/Scripts/ShopMenu.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class ShopMenu : MonoBehaviour
 {
     public string goShop;
+    public string goMain;
     // Start is called before the first frame update
     public void goToShop()
     {
         Application.LoadLevel(goShop);
+    }
+    
+    public void goToMain()
+    {
+        Application.LoadLevel(goMain);
     }
 }
