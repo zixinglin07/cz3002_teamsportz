49323826d5e6bbc8a04e8e5b1c042928369fb057
Chewposhi <chewposhi@gmail.com>
2022-03-14 20:55:18 +0800

    Added player attack

---
+++ TeamSportZ/Assets/BallDie.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDie : MonoBehaviour
+{
+    public GameObject diePEffect;
+    public float dieTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Timer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject collisionGameObject = collision.gameObject;
+
+        if(collisionGameObject.name != "Player")
+        {
+            Die();
+        }
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(dieTime);
+        Die();
+    }
+
+    void Die()
+    {
+        if(diePEffect != null)
+        {
+            Instantiate(diePEffect, transform.position, Quaternion.identity);
+        }
+        Destroy(gameObject);
+    }
+}
--- TeamSportZ/Assets/Scripts/Player/PlayerController.cs
+++ TeamSportZ/Assets/Scripts/Player/PlayerController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //for attack
+    private bool isShooting;
+    public float shootTimer;
+    public float shootSpeed;
+    public GameObject ball;
+    public Transform shootPos;
+
     public float moveSpeed;
     private float moveSpeedStore;
     public float jumpForce;
@@ -33,6 +40,9 @@ public class PlayerController : MonoBehaviour
         //speedMileStoneCountScore = speedMileStoneCount;
         //speedIncreaseMileStoneStore = speedIncreaseMilseStoneStore;
 
+        //attack
+        isShooting = false;
+
     }
 
     // Update is called once per frame
@@ -46,7 +56,7 @@ public class PlayerController : MonoBehaviour
        
 
         //jump if space is pressed or mouse is clicked
-        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        if(Input.GetKeyDown(KeyCode.Space))
         {
             if(grounded)
             {
@@ -55,7 +65,7 @@ public class PlayerController : MonoBehaviour
             
         }
 
-        if(Input.GetKey (KeyCode.Space) || Input.GetMouseButtonDown(0))
+        if(Input.GetKey (KeyCode.Space))
         {
             if(jumpTimeCounter > 0)
             {
@@ -64,7 +74,7 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        if(Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        if(Input.GetKeyUp(KeyCode.Space))
         {
             jumpTimeCounter = 0;
         }
@@ -76,6 +86,14 @@ public class PlayerController : MonoBehaviour
 
         myAnimator.SetFloat ("Speed", myRigidbody.velocity.x);
         myAnimator.SetBool ("Grounded", grounded);
+
+        //attack control
+        if(Input.GetMouseButtonDown(0) && !isShooting)
+        {
+            StartCoroutine(Shoot());
+
+        }
+
     }
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -93,5 +111,15 @@ public class PlayerController : MonoBehaviour
         moveSpeed = moveSpeedStore;
         myCollider.enabled = true;
         myRigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    //attack
+    IEnumerator Shoot()
+    {
+        isShooting = true;
+        GameObject newBall = Instantiate(ball, shootPos.position, Quaternion.identity);
+        newBall.GetComponent<Rigidbody2D>().velocity = new Vector2(shootSpeed * Time.fixedDeltaTime, 0f);
+        yield return new WaitForSeconds(shootTimer);
+        isShooting = false;
     }
 }
